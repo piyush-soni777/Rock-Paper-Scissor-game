65f202caf781f633e0b899986e2a2d32b746274e
PIYUSH SONI 🕸 <piyushsoni7777777@gmail.com>
2026-04-21 16:47:34 +0530

    Add user input for Rock, Paper, Scissors game

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ scissors = '''
 ---.__(___)
 '''
 
-
+#choose number which given to moves
 choice = (input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n0/1/2:"))
 computers_choice = random.randint(0,2)
 
